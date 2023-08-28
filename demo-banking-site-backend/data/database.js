0243734f34588cbfe25aaba3cf11cf4931ff9f55
Tharindu Ranaweera <ranaweerat@gmail.com>
2023-08-28 23:51:17 +0300

    Added create user POST endpoint

--- demo-banking-site-backend/data/database.js
+++ demo-banking-site-backend/data/database.js
@@ -15,13 +15,13 @@ const dbConfigWithDb = {
 };
 
 const USERS_TABLE_SQL =
-  "CREATE TABLE users (national_id VARCHAR(255) PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), address VARCHAR(255), sex VARCHAR(255), birthday DATE, profession VARCHAR(255), email VARCHAR(255))";
+  "CREATE TABLE users (national_id VARCHAR(255) PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), address VARCHAR(255), sex VARCHAR(255), birthday DATETIME, profession VARCHAR(255), email VARCHAR(255))";
 
 const ACCOUNTS_TABLE_SQL =
-  "CREATE TABLE accounts (account_number VARCHAR(255) PRIMARY KEY, account_type VARCHAR(255), balance DOUBLE, user_id VARCHAR(255), creation_date DATE, status VARCHAR(255))";
+  "CREATE TABLE accounts (account_number VARCHAR(255) PRIMARY KEY, account_type VARCHAR(255), balance DOUBLE, user_id VARCHAR(255), creation_date DATETIME, status VARCHAR(255))";
 
 const TRANSACTIONS_TABLE_SQL =
-  "CREATE TABLE transactions (transaction_id VARCHAR(255) PRIMARY KEY, transaction_type VARCHAR(255), account_id VARCHAR(255), amount DOUBLE, transaction_date DATE, status VARCHAR(255), recipient_account_id VARCHAR(255))";
+  "CREATE TABLE transactions (transaction_id VARCHAR(255) PRIMARY KEY, transaction_type VARCHAR(255), account_id VARCHAR(255), amount DOUBLE, transaction_date DATETIME, status VARCHAR(255), recipient_account_id VARCHAR(255))";
 
 const initConnection = mysql.createConnection(dbConfig);
 initConnection.query(
--- demo-banking-site-backend/index.js
+++ demo-banking-site-backend/index.js
@@ -1,13 +1,36 @@
 import express from "express";
+import { createDatabase } from "./data/database.js";
 
 const app = express();
 
 const PORT = 3001;
 
-app.get('/', (req, res) => {
-    res.send("Hi from server!");
-})
+app.use(express.json());
+
+app.post("/users/create", (req, res) => {
+  console.log(req.body);
+  const db = createDatabase();
+  const {
+    national_id,
+    firstname,
+    lastname,
+    address,
+    sex,
+    birthday,
+    profession,
+    status,
+  } = req.body;
+  const sql = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  db.query(sql, [national_id, firstname, lastname, address, sex, new Date(birthday).toISOString().slice(0, 19).replace("T", " "), profession, status], (error, result) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send();
+    }
+    res.status(200).send();
+  });
+
+});
 
 app.listen(PORT, () => {
-    console.log(`Server started on port: ${PORT}`);
+  console.log(`Server started on port: ${PORT}`);
 });
